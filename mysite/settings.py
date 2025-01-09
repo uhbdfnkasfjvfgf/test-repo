f977fb73425510d64ff9d1291b813fd983cabcb6
uhbdfnkasfjvfgf <bennjack999@outlook.com>
2025-01-09 18:32:49 +0600

    commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'kk-kz'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Almaty'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
